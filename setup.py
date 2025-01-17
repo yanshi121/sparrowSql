c11ae7af24953567ce3da63364b5bddccbf347fd
DY282 <2821963166@qq.com>
2025-01-17 09:41:15 +0800

    修改SqLite3导入错误

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("sparrowSql.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="sparrowSql",  # 包名
-    version="0.1.6",  # 包版本号
+    version="0.1.7",  # 包版本号
     author="39",  # 作者名
     author_email="DY39project@outlook.com",  # 联系方式
     description="基于各数据库连接包的二次开发SQL连接工具",  # 包的简述
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: POSIX :: Linux",
     ],
     python_requires='>=3.5',  # 对python的最低版本要求
-    install_requires=['pymysql', "dbutils", 'oracledb', 'psycopg2', 'requests', 'sqlite3', 'pyodbc']  # 项目依赖的其他库
+    install_requires=['pymysql', "dbutils", 'oracledb', 'psycopg2', 'requests', 'pyodbc']  # 项目依赖的其他库
 )
